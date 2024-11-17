10dada3232f3f09d95589eea333f07057c456ccd
MichelangeloCaltagirone <michelangelocaltagirone@gmail.com>
2024-11-17 11:44:52 +0100

    Modified the content of the Api's Response.

--- routes/api.php
+++ routes/api.php
@@ -21,5 +21,5 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 });
 
 
-Route::get("/post", [ProjectController::class, "index"])->name("api.projects.index");
+Route::get("/projects", [ProjectController::class, "index"])->name("api.projects.index");
 
